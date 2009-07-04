77d339431fb9b8a7ef55c0bf1938d4350a4d18f7
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-04 13:56:58 -0500

    added authors and email

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name = 'django-vcs',
     version = '0.1',
+    author = 'Alex Gaynor, Justin Lilly',
+    author_email = 'alex.gaynor@gmail.com',
     description = "A Django application to interface with a VCS using the pyvcs library.",
     url = 'http://github.com/alex/django-vcs/',
     packages = find_packages('django_vcs'),
