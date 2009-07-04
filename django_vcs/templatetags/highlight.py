ce4f69b5d0f46e6353e488671dd1954820a7b569
Justin Lilly <justinlilly@gmail.com>
2009-07-03 23:56:52 -0400

    Adding a default TextLexer for the highlighter when pygments can't find a default.

--- django_vcs/templatetags/highlight.py
+++ django_vcs/templatetags/highlight.py
@@ -3,15 +3,22 @@ from django.utils.safestring import mark_safe
 
 from pygments import highlight
 from pygments.lexers import guess_lexer_for_filename
+from pygments.lexers.special import TextLexer
+from pygments.util import ClassNotFound 
 from pygments.formatters import HtmlFormatter
 
 register = template.Library()
 
 @register.filter('highlight')
 def highlight_filter(text, filename):
+    try:
+        lexer = guess_lexer_for_filename(filename, text)
+    except ClassNotFound:
+        lexer = TextLexer()
+    
     return mark_safe(highlight(
         text,
-        guess_lexer_for_filename(filename, text),
+        lexer,
         HtmlFormatter()
     ))
 
