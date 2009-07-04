45df45c2c9bd1fa75d3f1f9653f12493705d6c9d
Alex Gaynor <alex.gaynor@gmail.com>
2009-07-04 13:31:19 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-vcs',
+    version = '0.1',
+    packages = find_packages('django_vcs'),
+    package_dir = {'': 'django_vcs'},
+    install_requires = ['setuptools'],
+    package_data = [
+        'templates/django_vcs/*.html',
+    ]
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Version Control',
+        'Framework :: Django',
+    ],
+)
