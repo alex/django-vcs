886f69249272f601114edd94590af1db329766c7
Justin Lilly <justinlilly@gmail.com>
2009-07-05 13:19:23 -0400

    Updating setup.py with more proper formatting.

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ setup(
     version = '0.1',
     author = 'Alex Gaynor, Justin Lilly',
     author_email = 'alex.gaynor@gmail.com',
-    description = "A Django application to interface with a VCS using the pyvcs library.",
+    description = "A pluggable django application to allow browsing of code repositories.",
     url = 'http://github.com/alex/django-vcs/',
-    packages = find_packages('django_vcs'),
-    package_data = {
-        'django_vcs': 'templates/django_vcs/*.html',
-    }
+    packages = find_packages(),
+    package_data = {'django_vcs': ['templates/django_vcs/*.html'],},
+    install_requires = ['pyvcs'],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development :: Version Control',
+        'Environment :: Web Environment',
         'Framework :: Django',
     ],
 )
